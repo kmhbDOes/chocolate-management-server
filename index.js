5c9c6c76f6597d0baf84462f5c9a9a3ba770f89f
kmhbDOes <kmhbipul@gmail.com>
2023-05-09 17:22:10 +0600

    Finished

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -26,8 +26,34 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        app.post('/coffee', async (req, res) => {
+        const chocolateCollection = client.db('chocolatedb').collection('chocolate')
 
+        app.get('/chocolate', async (req, res) => {
+            const cursor = chocolateCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/chocolate/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await chocolateCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.delete('/chocolate/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await chocolateCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        app.post('/chocolate', async (req, res) => {
+            const newChocolate = req.body;
+            console.log(newChocolate);
+            const result = await chocolateCollection.insertOne(newChocolate);
+            res.send(result)
         })
 
 
